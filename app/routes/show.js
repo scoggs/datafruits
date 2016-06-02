10071a1b52287eb10203b3d91ff8917392546255
Tony Miller <mcfiredrill@gmail.com>
2016-06-02 22:49:40 +0900

    add twitter cards for shows

--- app/routes/show.js
+++ app/routes/show.js
@@ -18,6 +18,59 @@ export default Ember.Route.extend({
     });
   },
 
+  afterModel: function(model) {
+   this.setHeadTags(model);
+  },
+
+  setHeadTags: function (model) {
+   var headTags = [
+     {
+       type: 'meta',
+       attrs: {
+         name: 'twitter:card',
+         content: 'summary_large_image'
+       },
+     },
+     {
+       type: 'meta',
+       attrs: {
+         name: 'twitter:site',
+         content: '@datafruits'
+       },
+     },
+     {
+       type: 'meta',
+       attrs: {
+         name: 'twitter:creator',
+         content: '@datafruits'
+       },
+     },
+     {
+       type: 'meta',
+       attrs: {
+         name: 'twitter:title',
+         content: model.title
+       },
+     },
+     {
+       type: 'meta',
+       attrs: {
+         name: 'twitter:description',
+         content: model.description
+       },
+     },
+     {
+       type: 'meta',
+       attrs: {
+         name: 'twitter:image',
+         content: model.image_url
+       },
+     },
+   ];
+
+   this.set('headTags', headTags);
+  },
+
   model: function(params) {
     return Ember.$.getJSON('http://datafruits.streampusher.com/scheduled_shows/'+params.id+'.json')
     .then(function(data){
